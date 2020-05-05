44a55b79dabff8a247cd8fe4f502f8529189576e
Magic-Mayo <mike.mayoii@gmail.com>
2020-05-05 02:30:01 -0700

    moved username state to app so it can be passed to other components. made route for game component and chat component to be rendered on

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,38 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import {Route} from 'react-router-dom';
 import NavBar from './components/NavBar';
 import Home from './pages/Home';
 import io from 'socket.io-client';
+import ChatBox from './components/ChatBox';
+import Game from './components/Game';
+import { Wrapper } from './components/styledComponents';
 
 const App = () => {
     const {current: socket} = useRef(io(':3001'));
+    const [username, setUsername] = useState();
 
     return (
         <>
             <NavBar/>
             <Route exact path='/'>
-                <Home socket={socket}/>
+                <Home
+                username={username}
+                setUsername={setUsername}
+                socket={socket}
+                />
+            </Route>
+            <Route path='/game'>
+                <Wrapper
+                w='95vw'
+                justifyContent='flex-end'
+                margin='80px 0 0 0'
+                >
+                    <Game />
+                    <ChatBox
+                    username={username}
+                    socket={socket}
+                    />
+                </Wrapper>
             </Route>
         </>
     );
