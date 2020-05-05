d9c51da7070ec3e1e751767cfd928b4bcc7cc215
Magic-Mayo <mike.mayoii@gmail.com>
2020-05-05 02:30:59 -0700

    added game and tray component. tray component renders a button for each letter

---
+++ client/src/components/Game/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Wrapper } from '../styledComponents';
+import Tray from './Tray';
+
+const Game = () => {
+    
+    return (
+        <Wrapper
+        w='50vw'
+        margin='0 100px'
+        >
+            <Tray />
+        </Wrapper>
+    )
+}
+
+export default Game;
