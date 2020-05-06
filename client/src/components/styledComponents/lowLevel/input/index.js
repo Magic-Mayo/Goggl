47d2b196e3f703bfc00b8869ce3e389f9c76766d
Magic-Mayo <mike.mayoii@gmail.com>
2020-05-06 03:03:33 -0700

    changed height to 35px

--- client/src/components/styledComponents/lowLevel/input/index.js
+++ client/src/components/styledComponents/lowLevel/input/index.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export default styled.input`
     width: ${({type, w}) => type === 'checkbox' ? '' : w ? w : '400px'};
-    height: 25px;
+    height: 35px;
     margin: ${({margin}) => margin || '10px'};
     padding: 5px;
     border-radius: 5px;
