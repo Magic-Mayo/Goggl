022109ae33edafd4ed5d8d37423f16580e038a8c
Magic-Mayo <mike.mayoii@gmail.com>
2020-05-06 02:32:06 -0700

    made functions to create and join rooms based on user input

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,29 +1,88 @@
 import React, {useState} from 'react';
-import {Wrapper, Input, Button} from '../components/styledComponents';
-import { useHistory } from 'react-router-dom';
-
+import {Wrapper, Input, Button, P} from '../components/styledComponents';
+import {useHistory} from 'react-router-dom';
+import Modal from '../components/Modal';
 
 const Home = ({socket, setUsername}) => {
     const history = useHistory();
-    const [input, setInput] = useState('');
+    const [input, setInput] = useState({
+        room: '',
+        username: ''
+    });
+    const [modal, setModal] = useState();
+    const [createOrJoin, setCreateOrJoin] = useState(false);
     
-    const handleUsername = () => {
-        socket.emit('set-username', input, chat => {
+    const handleJoinRoom = () => {
+        setCreateOrJoin(false);
+        if(!input.username){
+            return setInput();
+        }
+        
+        socket.emit('set-username', input.username, chat => {
             setUsername(chat);
-            history.push('/game');
+
+            socket.emit('join-room', input.room, create => {
+                if(create){
+                    return setModal(create);
+                }
+                
+                history.push('/game');
+            })
         });
+    }
+    
+    const handleCreateRoom = () => {
+        setCreateOrJoin(true);
+        if(!input.username){
+            return setInput();
+        }
+
+        socket.emit('set-username', input.username, chat => {
+            setUsername(chat);
+            socket.emit('create-room', input.room, join => {
+                if(join){
+                    return setModal(join);
+                }
+
+                history.push('/game');
+            })
+        })
+    }
+
+    const handleInput = e => {
+        const {name, value} = e.target;
+
+        setInput(prevInput => ({...prevInput, [name]: value}))
     }
 
     return (
         <Wrapper margin='80px 0 0 0'>
+            {modal && 
+                <Modal>
+                    <P
+                    fontS='18px'
+                    >
+                        {modal}
+                    </P>
+                    <Button
+                    w='200px'
+                    h='50px'
+                    fontS='22px'
+                    onClick={createOrJoin ? handleJoinRoom : handleCreateRoom}
+                    >
+                        {setCreateOrJoin ? 'Join Room' : 'Create Room'}
+                    </Button>
+                </Modal>
+            }
             <Wrapper>
                 <Input
-                onChange={e => setInput(e.target.value)}
-                value={input}
+                onChange={e => handleInput(e)}
+                value={input.username}
+                name='username'
                 />
                 <Button
                 type='button'
-                onClick={handleUsername}
+                onClick={handleCreateRoom}
                 >
                     Send name
                 </Button>
