4396a26f4b5e1ced4632b01a8093c76133cd6a2d
Magic-Mayo <mike.mayoii@gmail.com>
2020-05-05 22:27:32 -0700

    made component to display scores

---
+++ client/src/components/ScoreBox/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Wrapper } from '../styledComponents';
+
+const ScoreBox = () => {
+
+    return (
+        <Wrapper
+        w='400px'
+        h='700px'
+        bgColor='#456456'
+        border='2px solid #cdcdcd'
+        >
+
+        </Wrapper>
+    )
+}
+
+export default ScoreBox;
