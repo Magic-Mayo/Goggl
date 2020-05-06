fa4bdcc1a8482d9517484b1ca41837b030f6dd55
Magic-Mayo <mike.mayoii@gmail.com>
2020-05-05 22:27:32 -0700

    moved chatbox here and rendered a score box as well

--- client/src/pages/Game.js
+++ client/src/pages/Game.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import { Wrapper } from '../components/styledComponents';
+import GameTray from '../components/GameTray';
+import ScoreBox from '../components/ScoreBox';
+import ChatBox from '../components/ChatBox';
 
-const Game = () => {
+const Game = ({socket, username}) => {
 
     return (
-        <Wrapper>
-            
+        <Wrapper
+        w='100vw'
+        h='100vh'
+        justifyContent='space-evenly'
+        >
+            <ScoreBox />
+            <GameTray />
+            <ChatBox
+            username={username}
+            socket={socket}
+            />
         </Wrapper>
     )
 }
