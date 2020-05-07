a5f2b557c6e7cec46278a0545abb1a434ab051b8
Magic-Mayo <mike.mayoii@gmail.com>
2020-05-07 16:53:15 -0700

    added path for testing

--- server.js
+++ server.js
@@ -3,10 +3,11 @@ const http = require("http");
 const socketIo = require("socket.io");
 const app = express();
 const events = require('./sockets');
+const path = require('path');
 
 const PORT = process.env.PORT || 3001;
 
-app.get('*', (req, res) => res.sendFile(`${__dirname}/client/build/index.html`));
+app.get('*', (req, res) => res.sendFile(path.join(__dirname), './client/build/index.html'));
 
 const server = http.createServer(app);
 const io = socketIo(server);
