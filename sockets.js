aca68279cb639271b358c329b904efc08a137106
Magic-Mayo <mike.mayoii@gmail.com>
2020-05-07 04:14:39 -0700

    made function for emitting list of rooms that are open to join and created events to use it(refresh list, games list). disconnecting, joining and creating a room clears the interval on emitting the list. set upbroadcasting chats to room. set up loop to check for room name when joining or creating room so user knows a room already exists or doesnt

--- sockets.js
+++ sockets.js
@@ -1,18 +1,53 @@
-const roomnames = [];
+const findRooms = (io, socket) => {
+    const rooms = io.sockets.adapter.rooms;
 
-module.exports = socket => {
-    // set username
-    socket.on('set-username', (username, confirm) => {
-        socket.username = username;
-        confirm(socket.username)
-    });
+    if(!rooms){
+        return;
+    }
 
-    // create room
-    socket.on('create-room', (room, join) => {
-        if(roomnames.includes(room)) {
+    const ns = io.of("/");
+    const gameList = [];
+
+    for(let room in rooms){
+        const firstId = Object.keys(rooms[room].sockets)[0];
+        const firstUser = ns.connected[firstId].username;
+        gameList.push({room: room, creator: firstUser, numPlayers: rooms[room].length})
+    }
+
+    socket.emit('games-list', gameList);
+}
+
+module.exports = io => {
+
+    io.on('connection', socket =>{
+        socket.leave(socket.id);
+        findRooms(io, socket);
+
+        // set username
+        socket.on('set-username', (username, confirm) => {
+            socket.username = username;
+            confirm(socket.username)
+        });
+        
+        const emitGames = setInterval(() => {
+            findRooms(io, socket);
+        }, 60000);
+
+        socket.on('refresh-list', refresh => {
+            findRooms(io, socket);
+        })
+        
+        // create room
+        socket.on('create-room', (room, join) => {
             // If room exists will ask user to confirm if they want to try and join
-            join({msg: 'Room already exists.  Would you like to request access?', room: room})
-        } else {
+            for(let key in io.sockets.adapter.rooms){
+                if(key === room){
+                    return join({msg: 'Room already exists.  Would you like to request access?', room: room})
+
+                }
+            }
+
+            // Leave all other rooms before joining
             for(let key in socket.rooms){
                 if(key !== socket.room){
                     socket.leave(key, (err) => {
@@ -20,18 +55,24 @@ module.exports = socket => {
                     });
                 }
             }
+
             socket.join(room, () => {
-                roomnames.push(room);
+                clearInterval(emitGames);
                 socket.room = room;
                 join();
             });
-        }
-    });
-    
-    // join room
-    socket.on('join-room', (room, create) => {
-        if(roomnames.includes(room)){
-            socket.room = room;
+        });
+        
+        // join room
+        socket.on('join-room', (room, create) => {
+            // Sends mesage back to user if room doesn't exist.  Will create new room with the name they used if confirmed
+            for(let key in io.sockets.adapter.rooms){
+                if(key === room){
+                    return create({msg: 'Room does not exist.  Would you like to create it?', room: room})
+                }
+            }
+
+            // Leave all rooms before joining
             for(let key in socket.rooms){
                 if(key !== socket.room){
                     socket.leave(key, (err) => {
@@ -40,30 +81,32 @@ module.exports = socket => {
                     break;
                 }
             }
+
             socket.join(room, () => {
+                clearInterval(emitGames);
+                socket.room = room;
                 socket.to(room).emit('join', `${socket.username} has entered the room!`);
                 create();
             });
-        } else {
-            // Sends mesage back to user if room doesn't exist.  Will create new room with the name they used if confirmed
-            create({msg: 'Room does not exist.  Would you like to create it?', room: room})
-        }
-    });
+        });
 
-    socket.on('leave-room', confirmLeave => {
-        socket.leave(socket.room, () => {
-            confirmLeave(socket.room);
+        // Leave room
+        socket.on('leave-room', confirmLeave => {
+            socket.leave(socket.room, () => {
+                confirmLeave(socket.room);
+            });
+        })
+
+        // send messages
+        socket.on('chat', msg => {
+            socket.to(socket.room).emit('chat', {username: socket.username, msg: msg});
+        });
+
+        socket.on('disconnect', () => {
+            clearInterval(emitGames);
+            socket.leave(socket.room)
+            socket.broadcast.emit('user-disconnect', `${socket.username} has disconnected`);
+            socket.emit('You have disconnected')
         });
     })
-
-    // send messages
-    socket.on('chat', msg => {
-        socket.to(socket.room).emit('chat', {username: socket.username, msg: msg});
-    });
-
-    socket.on('disconnect', () => {
-        socket.leave(socket.room)
-        socket.broadcast.emit('user-disconnect', `${socket.username} has disconnected`);
-        socket.emit('You have disconnected')
-    });
 }
