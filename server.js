53ae9394edbdc1653c3277c75d1eb6b9f8198e18
Magic-Mayo <mike.mayoii@gmail.com>
2020-05-07 17:34:33 -0700

    added express static method for production

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ const path = require('path');
 
 const PORT = process.env.PORT || 3001;
 
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+}
+
 app.get('*', (req, res) => res.sendFile(path.join(__dirname, './client/build/index.html')));
 
 const server = http.createServer(app);
