bffcd7c58161bc9d23f4d3fb8249b576f1d76965
Magic-Mayo <mike.mayoii@gmail.com>
2020-05-09 01:55:50 -0700

    added context for socket logic and moved all socket listeners here

---
+++ client/src/utils/Context.js
@@ -0,0 +1,54 @@
+import React, {useState, useEffect, createContext} from 'react';
+import io from 'socket.io-client';
+
+let socket;
+
+if(process.env.NODE_ENV === 'production'){
+    socket = io();
+} else {
+    socket = io(':3001');
+}
+
+export const SocketContext = createContext(null);
+
+export default ({children}) => {
+    const [scores, setScores] = useState();
+    const [players, setPlayers] = useState();
+    const [username, setUsername] = useState();
+    const [games, setGames] = useState();
+    const [chat, setChat] = useState([]);
+
+
+    useEffect(() => {
+        socket.on('game-scores', score => {
+            setScores(score);
+        });
+
+        socket.on('games-list', gameList => {
+            setGames(gameList);
+        });
+
+        socket.on('chat', chat =>{
+            setChat(prevChat => [...prevChat, {msg: chat.msg, username: chat.username}]);
+        });
+
+        socket.on('join', findPlayers => {
+            setPlayers(prevPlayers => [...prevPlayers, findPlayers]);
+        });
+
+        return () => {
+            socket.off('game-scores');
+            socket.off('players-in-room');
+            socket.off('games-list');
+        }
+
+    }, [])
+
+    return (
+        <SocketContext.Provider
+        value={{socket, scores, players, username, games, chat, setScores, setPlayers, setUsername, setGames, setChat}}
+        >
+            {children}
+        </SocketContext.Provider>
+    )
+} 
