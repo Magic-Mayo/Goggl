9901b709c1a2ed63641064b4ba65d9386918b242
MagicMayo <mike.mayoii@gmail.com>
2020-05-17 15:55:12 -0700

    added context for updatedScores and added listener for when scores are sent from server

--- client/src/utils/Context.js
+++ client/src/utils/Context.js
@@ -12,13 +12,13 @@ if(process.env.NODE_ENV === 'production'){
 export const SocketContext = createContext(null);
 
 export default ({children}) => {
-    const [scores, setScores] = useState();
-    const [players, setPlayers] = useState();
-    const [username, setUsername] = useState();
-    const [games, setGames] = useState();
+    const [scores, setScores] = useState([]);
+    const [players, setPlayers] = useState([]);
+    const [username, setUsername] = useState('');
+    const [games, setGames] = useState([]);
     const [chat, setChat] = useState([]);
-
-
+    const [updatedScores, setUpdateScores] = useState([]);
+    
     useEffect(() => {
         socket.on('game-scores', score => {
             setScores(score);
@@ -28,7 +28,7 @@ export default ({children}) => {
             setGames(gameList);
         });
 
-        socket.on('chat', chat =>{
+        socket.on('chat', chat => {
             setChat(prevChat => [...prevChat, {msg: chat.msg, username: chat.username}]);
         });
 
@@ -36,17 +36,35 @@ export default ({children}) => {
             setPlayers(prevPlayers => [...prevPlayers, findPlayers]);
         });
 
+        socket.on('scores', playerScores => {
+            setUpdateScores(prevUpdatedScores => [...prevUpdatedScores, playerScores]);
+        })
+
         return () => {
             socket.off('game-scores');
             socket.off('players-in-room');
             socket.off('games-list');
         }
 
-    }, [])
+    }, []);
 
     return (
         <SocketContext.Provider
-        value={{socket, scores, players, username, games, chat, setScores, setPlayers, setUsername, setGames, setChat}}
+        value={{
+            socket,
+            scores,
+            updatedScores,
+            players,
+            username,
+            games,
+            chat,
+            setScores,
+            setUpdateScores,
+            setPlayers,
+            setUsername,
+            setGames,
+            setChat
+        }}
         >
             {children}
         </SocketContext.Provider>
