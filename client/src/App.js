e040dafa6a29a861d8ad8774d924877a1cd14fc5
Magic-Mayo <mike.mayoii@gmail.com>
2020-06-16 23:02:04 -0700

    moved socket provider to index.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,22 @@
-import React from 'react';
-import SocketContext from './utils/Context';
+import React, { useContext } from 'react';
+import {SocketContext} from './utils/Context';
 import {Route, Redirect} from 'react-router-dom';
 import Home from './pages/Home';
 import Game from './pages/Game';
 
 const App = () => {
+    const {username} = useContext(SocketContext);
 
     return (
-        <div>
-            <SocketContext>
-                <Route exact path='/'>
-                    <Home />
-                </Route>
-                <Route path='/game'>
-                    {/* {!username && <Redirect to='/' />} */}
-                    <Game />
-                </Route>
-            </SocketContext>
-        </div>
+        <>
+            <Route exact path='/'>
+                <Home />
+            </Route>
+            <Route path='/game'>
+                {!username && <Redirect to='/' />}
+                <Game />
+            </Route>
+        </>
     );
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -4,10 +4,13 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import {BrowserRouter as Router} from 'react-router-dom';
+import SocketContext from './utils/Context';
 
 ReactDOM.render(
     <Router>
-        <App />
+        <SocketContext>
+            <App />
+        </SocketContext>
     </Router>,
   document.getElementById('root')
 );
