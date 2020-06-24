b754fe47698ce4f0f3b8f6712017731b9e310842
Magic-Mayo <mike.mayoii@gmail.com>
2020-06-23 17:42:51 -0700

    changed hook to also include inner height

--- client/src/utils/hooks.js
+++ client/src/utils/hooks.js
@@ -1,13 +1,17 @@
 import {useEffect, useState} from 'react';
 
-export const useInnerWidth = () => {
+export const useWindowDimensions = () => {
     const [innerWidth, setInnerWidth] = useState(window.innerWidth);
+    const [innerHeight, setInnerHeight] = useState(window.innerHeight);
 
     useEffect(() => {
-        const widthListener = window.addEventListener('resize', () => setInnerWidth(window.innerWidth));
+        const setDimensions = window.addEventListener('resize', () => {
+            setInnerWidth(window.innerWidth);
+            setInnerHeight(window.innerHeight);
+        });
 
-        return () => window.removeEventListener('resize', widthListener);
+        return () => window.removeEventListener('resize', setDimensions);
     }, []);
 
-    return innerWidth;
+    return [innerWidth, innerHeight];
 }
