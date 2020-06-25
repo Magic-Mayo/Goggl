1f7bf2dc723d3b270d056e6faede5fc2be41fd8f
Magic-Mayo <mike.mayoii@gmail.com>
2020-06-24 22:17:52 -0700

    made modal for when user is inactive or disconnected. will set them to active or reconnect when they click a button

--- client/src/App.js
+++ client/src/App.js
@@ -3,21 +3,51 @@ import {SocketContext} from './utils/Context';
 import {Route, Redirect, Switch} from 'react-router-dom';
 import Home from './pages/Home';
 import Game from './pages/Game';
+import { P, Button } from './components/styledComponents';
 
 const App = () => {
-    const {username} = useContext(SocketContext);
+    const {username, isActive, notConnected, setIsActive, setNotConnected} = useContext(SocketContext);
+    const [isModalOpen, setIsModalOpen] = useState(false);
 
     return (
-        <Switch>
-            <Route exact path='/'>
-                <Home />
-            </Route>
-            <Route path='/game'>
-                {!username && <Redirect to='/' />}
-                <Game />
-            </Route>
-            <Redirect to='/' />
-        </Switch>
+        <>
+            {isModalOpen &&
+                <Modal
+                setIsModalOpen={setIsModalOpen}
+                >
+                    <P>
+                        {notConnected && 'You have been disconnected from the server for inactivity'}
+                        {!isActive && !notConnected && 'Are you still there??'}
+                    </P>
+
+                    {notConnected &&
+                        <Button
+                        onClick={() => }
+                        >
+                            Reconnect me
+                        </Button>
+                    }
+
+                    {!isActive && !notConnected &&
+                        <Button
+                        onClick={() => setIsActive(true)}
+                        >
+                            Still here!
+                        </Button>
+                    }
+                </Modal>
+            }
+            <Switch>
+                <Route exact path='/'>
+                    <Home />
+                </Route>
+                <Route path='/game'>
+                    {!username && <Redirect to='/' />}
+                    <Game />
+                </Route>
+                <Redirect to='/' />
+            </Switch>
+        </>    
     );
 }
 
