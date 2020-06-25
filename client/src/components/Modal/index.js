c992f2332bad698dcfeee4d01d31de38da39100b
Magic-Mayo <mike.mayoii@gmail.com>
2020-06-24 22:17:52 -0700

    made new wrapper component to be overlay to close modal

--- client/src/components/Modal/index.js
+++ client/src/components/Modal/index.js
@@ -1,23 +1,33 @@
 import React from 'react';
 import { Wrapper } from '../styledComponents';
 
-const Modal = ({children}) => {
+const Modal = ({children, setIsModalOpen}) => {
 
     return (
-        <Wrapper
-        position='fixed'
-        left='50%'
-        transForm='translateX(-50%)'
-        top='30vh'
-        w='300px'
-        h='150px'
-        bgColor='#3e1d5a'
-        borderRadius='10px'
-        flexDirection='column'
-        alignItems='center'
-        >
-            {children}
-        </Wrapper>
+        <>
+            <Wrapper
+            position='fixed'
+            w='100vw'
+            h='100vh'
+            onClick={() => setisModalOpen(false)}
+            />
+
+            <Wrapper
+            position='fixed'
+            zIndex='100'
+            left='50%'
+            transForm='translateX(-50%)'
+            top='30vh'
+            w='300px'
+            h='150px'
+            bgColor='#3e1d5a'
+            borderRadius='10px'
+            flexDirection='column'
+            alignItems='center'
+            >
+                {children}
+            </Wrapper>
+        </>
     )
 }
 
