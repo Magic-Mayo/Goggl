c8d5969666efe07de1bbda59561690f0ec792139
Magic-Mayo <mike.mayoii@gmail.com>
2020-07-30 17:26:43 -0700

    made use effect to open modal when timed out. styled modal. gave onclick for modal buttons

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,42 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import {SocketContext} from './utils/Context';
 import {Route, Redirect, Switch} from 'react-router-dom';
 import Home from './pages/Home';
 import Game from './pages/Game';
 import { P, Button } from './components/styledComponents';
+import Modal from './components/Modal';
 
 const App = () => {
-    const {username, isActive, notConnected, setIsActive, setNotConnected} = useContext(SocketContext);
+    const {username, isActive, notConnected, resetActive} = useContext(SocketContext);
     const [isModalOpen, setIsModalOpen] = useState(false);
+
+    useEffect(() => {
+        setIsModalOpen(() => {
+            if(!isActive || notConnected){
+                return true;
+            }
+        })
+    }, [isActive, notConnected])
 
     return (
         <>
             {isModalOpen &&
                 <Modal
-                setIsModalOpen={setIsModalOpen}
+                width='450px'
+                height='300px'
+                justify='space-evenly'
                 >
-                    <P>
+                    <P
+                    fontS='28px'
+                    >
                         {notConnected && 'You have been disconnected from the server for inactivity'}
                         {!isActive && !notConnected && 'Are you still there??'}
                     </P>
 
                     {notConnected &&
                         <Button
-                        onClick={() => }
+                        w='175px'
+                        onClick={resetActive}
                         >
                             Reconnect me
                         </Button>
@@ -30,7 +44,7 @@ const App = () => {
 
                     {!isActive && !notConnected &&
                         <Button
-                        onClick={() => setIsActive(true)}
+                        onClick={resetActive}
                         >
                             Still here!
                         </Button>
