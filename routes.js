9d735bb35b9162ac7596e5243bce981aac853137
Mike Mayo <mike_mayo@sweetwater.com>
2022-02-14 21:07:22 -0500

    use static

--- routes.js
+++ routes.js
@@ -2,6 +2,6 @@ import express from 'express';
 const path = '/home/pi/projects';
 
 export const routes = app => {
-    app.use(express.static('client/build'));
+    app.use(express.static('client/build/static'));
     app.get('/', (req, res) => res.sendFile(`${path}/Goggl/client/build/index.html`));
 }
